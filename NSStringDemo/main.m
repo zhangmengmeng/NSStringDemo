bf353d87c727ba74eb5514b2002d79c2268e2293
zhangmengmeng <1210118783@qq.com>
2015-05-15 22:19:01 +0800

    about string

---
+++ NSStringDemo/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  NSStringDemo
+//
+//  Created by qingyun on 15-1-17.
+//  Copyright (c) 2015年 hnqingyun.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    // 创造一个字符串
+    NSString *string = @"Hello!";
+    NSLog(@"%@", string);
+    
+    [string length];
+    return 0;
+}
+
